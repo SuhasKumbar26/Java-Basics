61aceaf16250837aa105e8ce1aaa077d071ccc2e
Suhas Kumbar <suhaskumbar1043@gmail.com>
2025-02-21 01:16:09 +0530

    Java basics

---
+++ Lecture Code/02 Java Basics/src/newLine.java
@@ -0,0 +1,11 @@
+public class newLine {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("EveryOne");
+        System.out.print("Nice to Meet you all\n");
+
+        System.out.println("Hello");
+        System.out.println("EveryOne");
+        System.out.println("Nice to Meet you All");
+    }
+}
